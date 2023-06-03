ae75aa875357b729126b23bf5069a4453df0e8d2
UzomaAkpala <francisakpala@yahoo.com>
2023-06-03 14:48:06 +0100

    sidebar update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Sidebar />
     </div>
   );
 }
